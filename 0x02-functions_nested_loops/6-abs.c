afd58617caaa8a0d6b0d8117a1e796167d51a18a
AbeebPro <mdahlat94@gmail.com>
2022-03-16 02:06:49 -0700

    My seventh commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints tha absolute value of an integer
+ * @r: a formal paramter of type int
+ *
+ * Return: Returns absolute value
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r*-1);
+}
