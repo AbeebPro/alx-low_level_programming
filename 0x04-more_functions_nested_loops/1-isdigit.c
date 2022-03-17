1cb741ff91231ddf297bfb2f6ed8f112a13c3a60
AbeebPro <mdahlat94@gmail.com>
2022-03-17 02:53:16 -0700

    My second commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isdigit - Checks for digit
+ * @c: A parameter of type int
+ *
+ * Return: 1 if digit and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	char i;
+	int num = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == c)
+			num = 1;
+		else
+			num = 0;
+	}
+	return (num);
+}
