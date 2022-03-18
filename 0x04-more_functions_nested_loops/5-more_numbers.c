573ce5e01c3af3fd7e62a8011c3d72c0197a7b74
AbeebPro <mdahlat94@gmail.com>
2022-03-18 04:35:57 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 0; ch < 15; ch++)
+		for (ch = 0; ch <= 14; ch++)
 		{
 			if (ch > 9)
-			{
 				_putchar(ch / 10 + '0');
-				_putchar(ch % 10 + '0');
-			}
-			else
-				_putchar(ch + '0');
+			_putchar(ch % 10 + '0');
 		}
 		_putchar('\n');
 	}
