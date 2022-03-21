d1b3af82ffeeb28e2896958511bf4a7ca7617899
AbeebPro <mdahlat94@gmail.com>
2022-03-21 09:52:12 -0700

    My second commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap two numbers
+ * @a: pointer to an int
+ * @b: pointer tonan int
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
