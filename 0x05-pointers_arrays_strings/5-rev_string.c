9163204325a2135d493a1caf8d5b00f1e6ecf85f
AbeebPro <mdahlat94@gmail.com>
2022-03-21 12:17:22 -0700

    My sixth commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reveese a string
+ * @s: A pointer to char
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0, len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	for (i = (len - i); i >= 0; i--)
+	{	
+		
+	}
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other char in a string
+ * @str: a pointer to a str
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (i / 2 == 0 || i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
