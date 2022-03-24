02ab2c537ffd96c9f1b41e5bdb749cb26a1a3a32
AbeebPro <mdahlat94@gmail.com>
2022-03-24 05:59:05 -0700

    My fourth commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, len = 0, j = 0, len2 = 0, val;
+	int i = 0, len = 0, j = 0, len2 = 0, val = 0;
 
 	while (s1[i] != '\0')
 	{
@@ -20,6 +20,8 @@ int _strcmp(char *s1, char *s2)
 		len2++;
 		j++;
 	}
+	if (s1 == s2)
+		val = 0;
 	for (i = 0; i < len; i++)
 	{
 		if (s1[i] > s2[i])
@@ -32,8 +34,6 @@ int _strcmp(char *s1, char *s2)
 			val = -15;
 			break;
 		}
-		else
-			val = 0;
 	}
 	return (val);
 }
