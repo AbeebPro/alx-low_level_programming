0a6d7d40434691c3605fdfeb48b35141d818026f
AbeebPro <mdahlat94@gmail.com>
2022-03-24 05:44:16 -0700

    My first commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strcmp - conpares two strings
+ * @s1: string
+ * @s2: string
+ *
+ * Return: an int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, len = 0, j = 0, len2 = 0, val;
+
+	while (s1[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		len2++;
+		j++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (s1[i] > s2[i])
+		{
+			val = 15;
+			break;
+		}
+		else if (s2[i] > s1[i])
+		{
+			val = -15;
+			break;
+		}
+		else
+			val = 0;
+	}
+	return (val);
+}
