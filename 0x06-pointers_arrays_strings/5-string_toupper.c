e8f53c77cdd8ca36e9bcb50acd703efc6d2f9d4b
AbeebPro <mdahlat94@gmail.com>
2022-03-24 04:23:20 -0700

    My third commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * string_toupper- converts a string to upper case
+ * @str: a char pointer
+ *
+ * Return: a pointer
+ */
+char *string_toupper(char *str)
+{
+	int i = 0, j, len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (j = 0; j < (len - j); j++)
+	{
+		if (str[j] == ' ')
+			str[j] = 32;
+		else if (str[j] >= 'a' && str[j] <= 'z')
+			str[j] = str[j] - 32;
+		else
+			str[j] = str[j];
+	}
+	return (str);
+}
