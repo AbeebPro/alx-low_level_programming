2f5d9e9472643ea49d1c98b15a025efc7a616cb4
AbeebPro <mdahlat94@gmail.com>
2022-03-28 02:31:50 -0700

    First commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset- fills the first n bytes of the memory area pointed to s
+ * with constant b
+ * @s: char pointer
+ * @b: specified string
+ * @n: unsigned int n
+ *
+ * Return: Return a pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
