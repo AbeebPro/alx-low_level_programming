29072e8ce5b2b907ec9e279f8bec37e0664cb166
AbeebPro <mdahlat94@gmail.com>
2022-03-30 09:59:26 -0700

    Task 4 power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,9 +12,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (x == 1)
-	{
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
 		return (1);
-	}
 	return (x * _pow_recursion(x, (y - 1)));
 }
