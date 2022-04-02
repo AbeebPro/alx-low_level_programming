4ff3895c9dc2276e64da80da459383bcc0f6260e
AbeebPro <mdahlat94@gmail.com>
2022-04-02 11:01:47 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,14 +6,11 @@
  * @argv: string array cobtaining arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i, sum = 0;
-
-	for (i = 1; i < argc; i++)
+	if (argc > 0)
 	{
-		sum += atoi(argv[i]);
+		printf("%d\n", argc - 1);
 	}
-	printf("%d\n", sum);
 	return (0);
 }
