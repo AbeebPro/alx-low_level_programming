6f447d92957e1723bf17b3418a8eb02089818883
Ahmad Abeeb <mdahlat94@gmail.com>
2022-04-02 16:31:07 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function name
+ * @argc: number of entry
+ * @argv: string array of entry
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
