38d25ca8438e4c4de8cdfd305af0148d9655aaca
AbeebPro <mdahlat94@gmail.com>
2022-04-08 06:25:43 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - function
+ * @b: unsigned int
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
