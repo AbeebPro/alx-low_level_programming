fe34e9a9fa57bd75c821edd356457a8768034ba7
AbeebPro <mdahlat94@gmail.com>
2022-04-23 05:24:57 -0700

    Linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("%u\n", h->len);
+		printf("[%u] ", h->len);
 		if (h->str == NULL)
-			printf("(nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("%s\n", h->str);
 		h = h->next;
