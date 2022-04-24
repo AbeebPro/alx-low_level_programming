6752a91156f1950c6fe5796dba60c8e9ec224992
AbeebPro <mdahlat94@gmail.com>
2022-04-24 02:23:23 -0700

    Linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,8 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[length])
 		length++;
 	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
 	newnode->len = length;
 	newnode->str = strdup(str);
 	newnode->next = *head;
