780230d78c8eddaeb02ffc93b7ebe2fdaf302125
AbeebPro <mdahlat94@gmail.com>
2022-04-27 06:29:11 -0700

    More linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-	new_node->n = n;
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
