a228464dc24ca68173110622acbce9bea5514ee4
AbeebPro <mdahlat94@gmail.com>
2022-05-04 00:29:16 -0700

    File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fdo, fdw, len = 0;
+
+	fdo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fdo < 0)
+		return (-1);
+	while (text_content[len])
+		len++;
+	fdw = write(fdo, text_content, len);
+	if (fdw < 0)
+		return (-1);
+	close(fdo);
+	return (1);
+}
