5d6424460a16297eab0321763ade8fe619792136
AbeebPro <mdahlat94@gmail.com>
2022-05-04 00:34:29 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fdo, fdw, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
 	fdo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fdo < 0)
 		return (-1);
