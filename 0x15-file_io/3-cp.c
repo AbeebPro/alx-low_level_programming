6b885484e909c8fddedf65e40c3b37bed3499a7a
AbeebPro <mdahlat94@gmail.com>
2022-05-05 08:16:52 -0700

    File i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,47 +8,43 @@
  */
 int main(int argc, char *argv[])
 {
-	int fdo_1, fdo_2, fdr, fdw, clfdo, clfdo2;
-	char buff[1024];
+	int fd_r, fd_w, x, m, n;
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdo_1 = open(argv[1], O_RDONLY);
-	if (fdo_1 < 0)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdo_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdo_2 < 0)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(98);
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
+			exit(99);
+		}
 	}
-	fdr = read(fdo_1, buff, 1024);
-	if (fdr < 0)
+	if (x < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	buff[fdr] = '\0';
-	fdw = write(fdo_2, buff, fdr);
-	if (fdw < 0)
+	m = close(fd_r);
+	n = close(fd_w);
+	if (m < 0 || n < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	clfdo = close(fdo_1);
-	clfdo2 = close(fdo_2);
-	if (clfdo < 0 || clfdo2 < 0)
-	{
-		if (clfdo < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdo_1);
-		if (clfdo2 < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdo_2);
+		if (m < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (n < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
